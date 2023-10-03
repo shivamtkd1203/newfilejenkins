1772dada497a0174acf6c1086d855f71519a8a73
shivamtkd1203 <125451891+shivamtkd1203@users.noreply.github.com>
2023-10-03 13:57:46 +0530

    Add files via upload

---
+++ appendstr.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class appendstr {
+    
+    public static void main(String[] args){
+        StringBuilder str = new StringBuilder("Shivam ");
+       
+        // System.out.println(str);
+        str.append("K");
+                str.append("u");
+                        str.append("m");
+                                str.append("a");
+                                        str.append("r");
+
+
+
+
+        
+
+        System.out.println(str);
+}
+}
